1fb4aa66874a6a6be5035fd66ec211ca3fca6708
Eduardo Fuentes <fuentes.eduardo19@gmail.com>
2020-10-19 16:39:41 -0700

    Added portfolio section

--- script.js
+++ script.js
@@ -4,10 +4,38 @@ var menuBtn = document.querySelector('.menu-icon');
 var navBar = document.querySelector('.nav-bar');
 var menuOpen = document.querySelector('.menu-img');
 var menuClose = document.querySelector('.menu-close')
-
-
+var about = document.getElementById('about');
+var portfolio = document.getElementById('portfolio');
+var contact = document.getElementById('contact');
+var home = document.getElementById('home');
 
 menuBtn.addEventListener('click', function(e) {
+    navBar.classList.toggle('toggleNavbar');
+    menuOpen.classList.toggle('menu-open')
+    menuClose.classList.toggle('menuCloseDisplay')
+    e.defaultPrevented;
+})
+
+about.addEventListener('click', function () {
+    navBar.classList.toggle('toggleNavbar');
+    menuOpen.classList.toggle('menu-open')
+    menuClose.classList.toggle('menuCloseDisplay')
+    e.defaultPrevented;
+})
+
+portfolio.addEventListener('click', function() {
+    navBar.classList.toggle('toggleNavbar');
+    menuOpen.classList.toggle('menu-open')
+    menuClose.classList.toggle('menuCloseDisplay')
+    e.defaultPrevented;
+})
+ contact.addEventListener('click', function() {
+    navBar.classList.toggle('toggleNavbar');
+    menuOpen.classList.toggle('menu-open')
+    menuClose.classList.toggle('menuCloseDisplay')
+    e.defaultPrevented;
+})
+home.addEventListener('click', function() {
     navBar.classList.toggle('toggleNavbar');
     menuOpen.classList.toggle('menu-open')
     menuClose.classList.toggle('menuCloseDisplay')
